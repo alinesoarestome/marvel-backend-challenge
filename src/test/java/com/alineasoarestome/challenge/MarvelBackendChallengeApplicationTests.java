2bb9a6fa12257eea4058e12ddc1cd919d39087be
Aline Tomé <alinesoarestome@gmail.com>
2020-12-18 17:38:07 -0300

    initial configs

---
+++ src/test/java/com/alineasoarestome/challenge/MarvelBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alineasoarestome.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
