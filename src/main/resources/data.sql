c4d0af5c873e9a7bfe0f2738e4e4facc73d84e31
Aline Tomé <alinesoarestome@gmail.com>
2020-12-19 00:44:40 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO CHARACTER (name,description,modified) VALUES ('Character1', 'Character1 description', CURRENT_TIMESTAMP);
+INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #141',141,'digital comic',1447,CURRENT_TIMESTAMP,'Comic 141',1);
+INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #607',607,'hardcover',3828,CURRENT_TIMESTAMP,'Comic 607',1);
 INSERT INTO CHARACTER (name,description,modified) VALUES ('Character2', 'Character2 description', CURRENT_TIMESTAMP);
+INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #442',442,'hardcover',5809,CURRENT_TIMESTAMP,'Comic 442',2);
+INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #891',891,'digest',6080,CURRENT_TIMESTAMP,'Comic 891',2);
