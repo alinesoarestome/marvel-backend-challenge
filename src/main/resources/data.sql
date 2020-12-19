4d69081029ad12def6f5a774da71504fc4ae0ed3
Aline Tomé <alinesoarestome@gmail.com>
2020-12-19 00:54:23 -0300

    Update h2 script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,12 @@
 INSERT INTO CHARACTER (name,description,modified) VALUES ('Character1', 'Character1 description', CURRENT_TIMESTAMP);
-INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #141',141,'digital comic',1447,CURRENT_TIMESTAMP,'Comic 141',1);
-INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #607',607,'hardcover',3828,CURRENT_TIMESTAMP,'Comic 607',1);
+INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #810',810,'COMIC',4890,CURRENT_TIMESTAMP,'Comic 810',1);
+INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #538',538,'TRADE_PAPERBACK',2739,CURRENT_TIMESTAMP,'Comic 538',1);
+INSERT INTO EVENT(description,title,start,end,modified,character_id) VALUES ('Event description #810','Event 810',CURRENT_DATE,CURRENT_DATE,CURRENT_TIMESTAMP,1);
+INSERT INTO EVENT(description,title,start,end,modified,character_id) VALUES ('Event description #538','Event 538',CURRENT_DATE,CURRENT_DATE,CURRENT_TIMESTAMP,1);
+INSERT INTO EVENT(description,title,start,end,modified,character_id) VALUES ('Event description #253','Event 253',CURRENT_DATE,CURRENT_DATE,CURRENT_TIMESTAMP,1);
 INSERT INTO CHARACTER (name,description,modified) VALUES ('Character2', 'Character2 description', CURRENT_TIMESTAMP);
-INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #442',442,'hardcover',5809,CURRENT_TIMESTAMP,'Comic 442',2);
-INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #891',891,'digest',6080,CURRENT_TIMESTAMP,'Comic 891',2);
+INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #540',540,'GRAPHIC_NOVEL',4762,CURRENT_TIMESTAMP,'Comic 540',2);
+INSERT INTO COMIC(description,digital_id,format,issue_number,modified,title,character_id) VALUES ('Comic description #588',588,'DIGITAL_COMIC',3113,CURRENT_TIMESTAMP,'Comic 588',2);
+INSERT INTO EVENT(description,title,start,end,modified,character_id) VALUES ('Event description #540','Event 540',CURRENT_DATE,CURRENT_DATE,CURRENT_TIMESTAMP,2);
+INSERT INTO EVENT(description,title,start,end,modified,character_id) VALUES ('Event description #588','Event 588',CURRENT_DATE,CURRENT_DATE,CURRENT_TIMESTAMP,2);
+INSERT INTO EVENT(description,title,start,end,modified,character_id) VALUES ('Event description #235','Event 235',CURRENT_DATE,CURRENT_DATE,CURRENT_TIMESTAMP,2);
