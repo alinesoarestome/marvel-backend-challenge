969f72763a432c4ac61ef59da6c4085f249fbb1d
Aline Tomé <alinesoarestome@gmail.com>
2020-12-19 01:17:04 -0300

    Create story service

--- src/main/java/com/alineasoarestome/challenge/resource/CharacterResource.java
+++ src/main/java/com/alineasoarestome/challenge/resource/CharacterResource.java
@@ -13,10 +13,12 @@ import com.alineasoarestome.challenge.domain.Character;
 import com.alineasoarestome.challenge.domain.Comic;
 import com.alineasoarestome.challenge.domain.Event;
 import com.alineasoarestome.challenge.domain.Serie;
+import com.alineasoarestome.challenge.domain.Story;
 import com.alineasoarestome.challenge.service.CharacterService;
 import com.alineasoarestome.challenge.service.ComicService;
 import com.alineasoarestome.challenge.service.EventService;
 import com.alineasoarestome.challenge.service.SerieService;
+import com.alineasoarestome.challenge.service.StoryService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,16 +29,18 @@ public class CharacterResource {
 
     @Autowired
     private CharacterService characterService;
-    
-    @Autowired 
+
+    @Autowired
     private ComicService comicService;
-    
-    @Autowired 
+
+    @Autowired
     private EventService eventService;
-    
-    @Autowired 
+
+    @Autowired
     private SerieService serieService;
 
+    @Autowired
+    private StoryService storyService;
 
     @GetMapping
     public ResponseEntity<List<Character>> listAll() {
@@ -51,47 +55,56 @@ public class CharacterResource {
 
     @GetMapping("/{id}")
     public ResponseEntity<Character> getById(@PathVariable("id") Integer id) {
-	log.info("getById(): fetching character [id = " +id+ "].");
+	log.info("getById(): fetching character [id = " + id + "].");
 
 	Character character = characterService.getById(id);
-	
-	log.info("getById(): character returned.");
 
+	log.info("getById(): character returned.");
 
 	return ResponseEntity.ok(character);
     }
 
-
     @GetMapping("/{id}/comics")
     public ResponseEntity<List<Comic>> getComicsById(@PathVariable("id") Integer id) {
-	log.info("getComicsById(): fetching comics by character [id = " +id+ "].");
-	
+	log.info("getComicsById(): fetching comics by character [id = " + id + "].");
+
 	List<Comic> comics = comicService.listAllByCharacterId(id);
-	
+
 	log.info("getComicsById(): " + comics.size() + " comics returned.");
-	
+
 	return ResponseEntity.ok(comics);
     }
-    
+
     @GetMapping("/{id}/events")
     public ResponseEntity<List<Event>> getEventById(@PathVariable("id") Integer id) {
-	log.info("getComicsById(): fetching events by character [id = " +id+ "].");
-	
+	log.info("getEventById(): fetching events by character [id = " + id + "].");
+
 	List<Event> events = eventService.listAllByCharacterId(id);
-	
-	log.info("getComicsById(): " + events.size() + " events returned.");
-	
+
+	log.info("getEventById(): " + events.size() + " events returned.");
+
 	return ResponseEntity.ok(events);
     }
-    
+
     @GetMapping("/{id}/series")
     public ResponseEntity<List<Serie>> getSeriesById(@PathVariable("id") Integer id) {
-	log.info("getComicsById(): fetching series by character [id = " +id+ "].");
-	
+	log.info("getSeriesById(): fetching series by character [id = " + id + "].");
+
 	List<Serie> series = serieService.listAllByCharacterId(id);
-	
-	log.info("getComicsById(): " + series.size() + " events returned.");
-	
+
+	log.info("getSeriesById(): " + series.size() + " events returned.");
+
 	return ResponseEntity.ok(series);
+    }
+
+    @GetMapping("/{id}/stories")
+    public ResponseEntity<List<Story>> getStoriesById(@PathVariable("id") Integer id) {
+	log.info("getStoriesById(): fetching stories by character [id = " + id + "].");
+
+	List<Story> stories = storyService.listAllByCharacterId(id);
+
+	log.info("getStoriesById(): " + stories.size() + " events returned.");
+
+	return ResponseEntity.ok(stories);
     }
 }
